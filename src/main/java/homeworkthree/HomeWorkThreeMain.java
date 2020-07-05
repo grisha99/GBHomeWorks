1821dac4798e8d297aa4a64d6296c6dc6843d95c
Grishchenko <grishenko_sy@mail.ru>
2020-07-05 15:56:37 +0300

    finish home work three

---
+++ src/main/java/homeworkthree/HomeWorkThreeMain.java
@@ -0,0 +1,11 @@
+package homeworkthree;
+
+import homeworkthree.HomeWorkThreeTasks;
+
+public class HomeWorkThreeMain {
+
+    public static void main(String[] args) {
+        //HomeWorkThreeTasks hwt = new HomeWorkThreeTasks(); // объект для проверки
+
+    }
+}
