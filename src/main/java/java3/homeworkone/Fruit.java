717c69b3247e478884b810cd1b3fd4daa691af95
Grishchenko <grishenko_sy@mail.ru>
2020-08-31 00:25:04 +0300

    finish home work one, Java-3

---
+++ src/main/java/java3/homeworkone/Fruit.java
@@ -0,0 +1,21 @@
+package java3.homeworkone;
+
+public class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    /**
+     *
+     * Получить вес фрукта
+     *
+     * @return  вес фрукта
+     */
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/java3/homeworkone/Orange.java
@@ -0,0 +1,14 @@
+package java3.homeworkone;
+
+public class Orange extends Fruit {
+
+    private final static float ORANGE_WEIGHT_DEFAULT = 1.5f;
+
+    public Orange() {
+        super(ORANGE_WEIGHT_DEFAULT);
+    }
+
+    public Orange(float weight) {
+        super(weight);
+    }
+}
